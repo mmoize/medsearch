c0e1487e9f3da12fc66e662ed7ddc53a7e67c8a9
Moise Murhi <murhi46@gmail.com>
2020-11-25 15:00:40 +1100

    Adding adminstration account logic

--- account/models.py
+++ account/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import os
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+DEFAULT = 'default.jpg'
+
+#Save's Admins Display Image.
+def get_image_path(instance, filename):
+    return os.path.join('DP', str(instance.user), filename)
+
+
+class  Owner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    company = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(upload_to= get_image_path, default=DEFAULT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+    # The Owner will be defined by username field.
+    def __str__(self):
+        return self.user.username
+
+
+# Signaling logic for triggering Owner object creation after signing up.
+def create_user_owner(sender, instance, created, **kwargs):
+    if created:
+        owner = Owner(user=instance)
+        owner.save()
+post_save.connect(create_user_owner, sender=User, dispatch_uid="users-ownercreation-signal")
--- medication/models.py
+++ medication/models.py
@@ -3,3 +3,4 @@
 # from django.contrib.auth.models import User
 # from django_extensions.db.models import (ActivatorModel,TimeStampedModel)
 # from django_resized import ResizedImageField
+
